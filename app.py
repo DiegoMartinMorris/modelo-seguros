acfdf7b60b0501eb77d059ecb267906c6c9a11e2
DiegoMartinMorris <morrisdiegom@gmail.com>
2026-04-22 20:16:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,10 @@ st.markdown("""
 
 
 def conectar_google_sheet():
-    scope = ["https://www.googleapis.com/auth/spreadsheets"]
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
     creds = Credentials.from_service_account_info(
         st.secrets["gcp_service_account"], scopes=scope
     )
